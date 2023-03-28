b6c4b172380216c3a16646b444435ce06b60e03e
Robert Carter Mills <robcmills@gmail.com>
2023-03-27 20:59:38 -0700

    Fix LanguageSelect focus issue

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,5 +1,6 @@
 import { CODLES } from "codle/codle/getRandomCodle";
 import { type Language } from "codle/types/Language";
+import { useRef } from "react";
 
 export function LanguageSelect({
   language,
@@ -8,6 +9,8 @@ export function LanguageSelect({
   language: string;
   onChange: (language: Language) => void;
 }) {
+  const selectRef = useRef<HTMLSelectElement>(null);
+
   const selectClassName =
     "bg-[#343440] text-white rounded-md p-2 border-x-8 border-[#343440]";
   const optionClassName = "bg-[#343440] text-white";
@@ -18,12 +21,22 @@ export function LanguageSelect({
     </option>
   ));
 
+  const _onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(e.target.value as Language);
+    if (selectRef.current) {
+      selectRef.current.blur();
+      // else the select will stay focused and keyboard events will
+      // change the language unexpectedly (on desktop)
+    }
+  };
+
   return (
     <div className="grid justify-center">
       <select
         className={selectClassName}
         value={language}
-        onChange={(e) => onChange(e.target.value as Language)}
+        onChange={_onChange}
+        ref={selectRef}
       >
         {options}
       </select>
