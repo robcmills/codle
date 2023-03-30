81cb96fff73f40cc1bc108626c45a935e95d857b
Robert Carter Mills <robcmills@gmail.com>
2023-03-29 22:50:24 -0700

    Use next/image component for UserAvatar

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,13 +1,16 @@
 import type { UserResource } from "@clerk/types";
+import Image from "next/image";
 
 export function UserAvatar({ user }: { user?: UserResource | null }) {
   if (!user) return null;
   return (
     <div className="grid place-items-center">
-      <img
+      <Image
         className="h-8 w-8 rounded-full"
         src={user.profileImageUrl}
         alt={user.username || "User Profile Image"}
+        height={32}
+        width={32}
       />
     </div>
   );
