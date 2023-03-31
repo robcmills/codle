da97f652a1e4fa58e73519428a3f5d85dc3bef28
Robert Carter Mills <robcmills@gmail.com>
2023-03-31 11:14:21 -0700

    Ensure signed in before routing to existing game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,26 @@ import { type NextPage } from "next";
 import { Instructions } from "codle/components/Instructions";
 import { api } from "codle/utils/api";
 import { useRouter } from "next/router";
+import { useUser } from "@clerk/nextjs";
 
 const IndexPage: NextPage = () => {
   const router = useRouter();
-
-  const { data: game, isLoading } = api.game.get.useQuery();
+  const { isSignedIn } = useUser();
+  const { data: game, isLoading } = api.game.get.useQuery(undefined, {
+    enabled: !!isSignedIn,
+  });
 
   const onClickPlay = () => {
-    if (!game) throw new Error("No game to play");
-    router.push(`/games/${game.id}`).catch(console.error);
+    if (isSignedIn && game) {
+      router.push(`/games/${game.id}`).catch(console.error);
+    } else {
+      router.push("/play").catch(console.error);
+    }
   };
 
-  return <Instructions {...{ onClickPlay, isLoading }} />;
+  return (
+    <Instructions {...{ onClickPlay, isLoading: !!isSignedIn && isLoading }} />
+  );
 };
 
 export default IndexPage;
---
+++ src/pages/play.tsx
@@ -0,0 +1,7 @@
+import { type NextPage } from "next";
+
+const PlayPage: NextPage = () => {
+  return <div>Play</div>;
+};
+
+export default PlayPage;
