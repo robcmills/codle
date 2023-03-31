e5381b3e5e98fc0d5fbd4b76a7865b65610e9859
Robert Carter Mills <robcmills@gmail.com>
2023-03-31 09:32:37 -0700

    Refactor getGame to create new game if no existing found

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,12 @@
 import { type NextPage } from "next";
 import { Instructions } from "codle/components/Instructions";
 import { api } from "codle/utils/api";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 const IndexPage: NextPage = () => {
   const router = useRouter();
 
-  const { data: existingGame, isFetched } = api.game.get.useQuery();
-  const {
-    data: newGame,
-    mutate: createGame,
-    isLoading: isCreating,
-  } = api.game.create.useMutation();
-
-  useEffect(() => {
-    if (isFetched && !existingGame && !isCreating && !newGame) {
-      createGame({ language: "JavaScript" });
-    }
-  }, [createGame, existingGame, isCreating, isFetched, newGame]);
-
-  const game = existingGame || newGame;
+  const { data: game } = api.game.get.useQuery();
 
   const onClickPlay = () => {
     if (!game) throw new Error("No game to play");
--- src/server/api/getGame.ts
+++ src/server/api/getGame.ts
@@ -1,6 +1,10 @@
+import { getRandomCodle } from "codle/codle/getRandomCodle";
 import { privateProcedure } from "codle/server/api/trpc";
 
-/* Get the most recent unsolved game for the current user. */
+/**
+ * Get the most recent unsolved game for the current user.
+ * If none exist, create a new game.
+ **/
 export const getGame = privateProcedure.query(async ({ ctx }) => {
   const playerId = ctx.userId;
   const games = await ctx.prisma.game.findMany({
@@ -8,9 +12,23 @@ export const getGame = privateProcedure.query(async ({ ctx }) => {
     orderBy: { updatedAt: "desc" },
     include: { guesses: true },
   });
-  return games.find(
+  const existingGame = games.find(
     (game) =>
       game.guesses.length === 0 ||
       game.guesses.every((guess) => guess.letters !== game.codle)
   );
+  if (existingGame) return existingGame;
+
+  const language = "JavaScript";
+  // Ensure chosen codle has not been used before
+  const usedCodles = games.map((game) => game.codle);
+  const codle = getRandomCodle({ language, exclude: usedCodles });
+  const newGame = await ctx.prisma.game.create({
+    data: {
+      codle,
+      language,
+      playerId,
+    },
+  });
+  return newGame;
 });
